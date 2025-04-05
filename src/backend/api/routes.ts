55d6181e634e414a0c7c0f3b18841105099339cd
Manling <727500834@qq.com>
2025-04-06 11:54:33 +1200

    feat: set up TSOA config and minimal Problem endpoints

---
+++ src/backend/api/routes.ts
@@ -0,0 +1,130 @@
+/* tslint:disable */
+/* eslint-disable */
+// WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+import type { TsoaRoute } from '@tsoa/runtime';
+import {  fetchMiddlewares, ExpressTemplateService } from '@tsoa/runtime';
+// WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+import { ProblemsController } from './problems/problem-controller';
+import type { Request as ExRequest, Response as ExResponse, RequestHandler, Router } from 'express';
+
+
+
+// WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+
+const models: TsoaRoute.Models = {
+    "_36_Enums.Difficulty": {
+        "dataType": "refAlias",
+        "type": {"dataType":"union","subSchemas":[{"dataType":"enum","enums":["EASY"]},{"dataType":"enum","enums":["MEDIUM"]},{"dataType":"enum","enums":["HARD"]}],"validators":{}},
+    },
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+    "Difficulty": {
+        "dataType": "refAlias",
+        "type": {"ref":"_36_Enums.Difficulty","validators":{}},
+    },
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+    "ProblemSummary": {
+        "dataType": "refObject",
+        "properties": {
+            "problemId": {"dataType":"double","required":true},
+            "title": {"dataType":"string","required":true},
+            "difficulty": {"ref":"Difficulty","required":true},
+        },
+        "additionalProperties": true,
+    },
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+    "ProblemDetails": {
+        "dataType": "refObject",
+        "properties": {
+            "problemId": {"dataType":"double","required":true},
+            "title": {"dataType":"string","required":true},
+            "difficulty": {"ref":"Difficulty","required":true},
+            "description": {"dataType":"string","required":true},
+            "createdAt": {"dataType":"string","required":true},
+        },
+        "additionalProperties": true,
+    },
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+};
+const templateService = new ExpressTemplateService(models, {"noImplicitAdditionalProperties":"ignore","bodyCoercion":true});
+
+// WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+
+
+
+
+export function RegisterRoutes(app: Router) {
+
+    // ###########################################################################################################
+    //  NOTE: If you do not see routes for all of your controllers in this file, then you might not have informed tsoa of where to look
+    //      Please look into the "controllerPathGlobs" config option described in the readme: https://github.com/lukeautry/tsoa
+    // ###########################################################################################################
+
+
+    
+        const argsProblemsController_getAllProblems: Record<string, TsoaRoute.ParameterSchema> = {
+        };
+        app.get('/problems',
+            ...(fetchMiddlewares<RequestHandler>(ProblemsController)),
+            ...(fetchMiddlewares<RequestHandler>(ProblemsController.prototype.getAllProblems)),
+
+            async function ProblemsController_getAllProblems(request: ExRequest, response: ExResponse, next: any) {
+
+            // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+
+            let validatedArgs: any[] = [];
+            try {
+                validatedArgs = templateService.getValidatedArgs({ args: argsProblemsController_getAllProblems, request, response });
+
+                const controller = new ProblemsController();
+
+              await templateService.apiHandler({
+                methodName: 'getAllProblems',
+                controller,
+                response,
+                next,
+                validatedArgs,
+                successStatus: undefined,
+              });
+            } catch (err) {
+                return next(err);
+            }
+        });
+        // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+        const argsProblemsController_getProblem: Record<string, TsoaRoute.ParameterSchema> = {
+                problemId: {"in":"path","name":"problemId","required":true,"dataType":"double"},
+        };
+        app.get('/problems/:problemId',
+            ...(fetchMiddlewares<RequestHandler>(ProblemsController)),
+            ...(fetchMiddlewares<RequestHandler>(ProblemsController.prototype.getProblem)),
+
+            async function ProblemsController_getProblem(request: ExRequest, response: ExResponse, next: any) {
+
+            // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+
+            let validatedArgs: any[] = [];
+            try {
+                validatedArgs = templateService.getValidatedArgs({ args: argsProblemsController_getProblem, request, response });
+
+                const controller = new ProblemsController();
+
+              await templateService.apiHandler({
+                methodName: 'getProblem',
+                controller,
+                response,
+                next,
+                validatedArgs,
+                successStatus: undefined,
+              });
+            } catch (err) {
+                return next(err);
+            }
+        });
+        // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+
+
+    // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+}
+
+// WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
