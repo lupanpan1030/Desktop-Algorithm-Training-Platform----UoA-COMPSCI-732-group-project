fae2469f5610e7ada2377c9051fd2a1da3ff070f
YMKT-951 <byhz331@aucklanduni.ac.nz>
2025-05-14 13:46:46 +1200

    fix: resolve the bug of deleting language

--- src/frontend/pages/LanguageAdmin.tsx
+++ src/frontend/pages/LanguageAdmin.tsx
@@ -88,6 +88,7 @@ export default function LanguageAdmin() {
 
     try {
       await deleteLanguage(del.id);
+      await fetchLanguages();
       notify(strings.deleteSuccess(del.name));
     } catch (e: any) {
       notify(e.message, 'error');
@@ -95,7 +96,7 @@ export default function LanguageAdmin() {
       setDel({ open: false, id: null, name: '' });
       setShowDelete(false);
     }
-  }, [del, deleteLanguage]);
+  }, [del, deleteLanguage, fetchLanguages]);
 
 // Render (渲染)
   return (
